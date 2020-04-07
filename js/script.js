4d57def81e3c9874b611757a979702b5cca0c9c6
Orlov Ilya <EagleCanFly>
2020-04-07 17:53:26 +0300

    added some styling and sort

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const searchBtn = document.getElementById('search-btn');
 let inputMovie = document.querySelector('#input-movie');
-const poster = document.querySelector('.posters');
+const poster = $('.posters');
 
 // Data
 const apiHost = 'https://api.themoviedb.org';
@@ -10,22 +10,44 @@ const apiKey = 'd497820dd50143376ccc71f1b2148479';
 
 
 // Functions
-searchBtn.addEventListener('click', () => {
 
+const clickHandler = () => {
     const response = getMovie();
     response.then(onDataReceived);
+
+};
+
+// inputMovie.onkeypress = clickHandler;
+
+searchBtn.addEventListener('click', () => {
+    poster.empty();
+    clickHandler();
 });
+
 
 const onDataReceived = (response) => {
     console.log(response);
     poster.innerHTML = '';
-    response.results.forEach(film => {
+    const fismsSort = response.results.sort(function (a, b) {
+        if (a.popularity < b.popularity) {
+          return 1;
+        }
+        if (a.popularity > b.popularity) {
+          return -1;
+        }
+        // a должно быть равным b
+        return 0;
+      });
+    fismsSort.forEach((film, item) => {
         if (film.poster_path === null) return;
+        $(`<div class="block-${item}"></div>`).appendTo(poster);
+        const block = $(`.block-${item}`);
+        $(`<p class="title">${film.title}</p>`).appendTo(block);
         const img = document.createElement('img');
         img.style.padding = '10px';
         const posterLink = imageHost + film.poster_path;
         img.src = posterLink
-        poster.append(img);
+        block.append(img);
     });
 
 };
